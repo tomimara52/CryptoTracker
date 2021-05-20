b6752b098feaaae7ef98f26d37a2bceaadd553b4
Tomás Maraschio <tomas.maraschio@gmail.com>
2021-05-19 23:35:55 -0300

    changed from requests library to pycurl

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
-import requests
 import matplotlib.pyplot as plt
 from datetime import datetime
 import pandas as pd
 from dateutil.relativedelta import *
+import pycurl
+from io import BytesIO
+import json
 
 
-def get_crypto_value(base_currency="BTC", currency="USD", target_date=None, session=None):
-    params = {}
-    if target_date is not None:
-        params = {'date': target_date}
+def get_crypto_value(base_currency="BTC", currency="USD", target_date=None, curl=None):
 
     url = f"https://api.coinbase.com/v2/prices/{base_currency}-{currency}/spot"
 
-    if session is None:
-        response = requests.get(url, params=params).json()
-    else:
-        response = session.get(url, params=params).json()
+    if target_date is not None:
+        url += '?date=' + target_date
+
+    buffer = BytesIO()
+
+    if curl is None:
+        curl = pycurl.Curl()
+
+    curl.setopt(pycurl.URL, url)
+    curl.setopt(pycurl.WRITEDATA, buffer)
+    curl.setopt(pycurl.SSL_VERIFYPEER, 0)
+    curl.perform()
+    response = buffer.getvalue()
+    response = json.loads(response.decode("iso-8859-1"))
 
     if response.get("errors"):
         raise Exception(response['errors'][0]["message"])
@@ -34,21 +43,22 @@ def crypto_graphic(start_date, end_date=datetime.today(), days_interval=30, base
     date_format = "%Y-%m-%d"
 
     # while 'new_date' is less than 'end_date' keep adding dates and amounts to 'date_time' and 'data'.
-    with requests.Session() as session:
-        while True:
-            new_date = datetime.strptime(date_time[-1], date_format) + relativedelta(days=days_interval)
 
-            if new_date > end_date:
-                end_date_str = end_date.strftime(date_format)
-                date_time.append(end_date_str)
-                amount = get_crypto_value(base_currency, currency, end_date_str, session)[-1]
-                data.append(float(amount))
-                break
+    curl = pycurl.Curl()
+    while True:
+        new_date = datetime.strptime(date_time[-1], date_format) + relativedelta(days=days_interval)
 
-            new_date_str = new_date.strftime(date_format)
-            date_time.append(new_date_str)
-            amount = get_crypto_value(base_currency, currency, new_date_str, session)[-1]
+        if new_date > end_date:
+            end_date_str = end_date.strftime(date_format)
+            date_time.append(end_date_str)
+            amount = get_crypto_value(base_currency, currency, end_date_str, curl)[-1]
             data.append(float(amount))
+            break
+
+        new_date_str = new_date.strftime(date_format)
+        date_time.append(new_date_str)
+        amount = get_crypto_value(base_currency, currency, new_date_str, curl)[-1]
+        data.append(float(amount))
 
     # create a data frame with the amounts in the rows and the dates as index. Then plot it.
     date_time = pd.to_datetime(date_time, yearfirst=True)
@@ -61,5 +71,5 @@ def crypto_graphic(start_date, end_date=datetime.today(), days_interval=30, base
 
 
 
-crypto_graphic("2021-02-02", days_interval=1, base_currency="BTC")
-# print(get_crypto_value(target_date='2021-05-19'))
+crypto_graphic("2021-02-02", days_interval=1, base_currency="ETH")
+# print(get_crypto_value())
