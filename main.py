38200bdba621bd066909b7279d2c5798548393a6
Tomás Maraschio <tomas.maraschio@gmail.com>
2021-05-20 13:58:09 -0300

    implemented asynchronous requests for optimizing the process

--- main.py
+++ main.py
@@ -2,63 +2,59 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import pandas as pd
 from dateutil.relativedelta import *
-import pycurl
-from io import BytesIO
 import json
+import asyncio
+import aiohttp
 
 
-def get_crypto_value(base_currency="BTC", currency="USD", target_date=None, curl=None):
+async def get_crypto_value(session, base_currency="BTC", currency="USD", target_date=None):
 
     url = f"https://api.coinbase.com/v2/prices/{base_currency}-{currency}/spot"
 
     if target_date is not None:
         url += '?date=' + target_date
 
-    buffer = BytesIO()
+    async with session.get(url) as response:
 
-    if curl is None:
-        curl = pycurl.Curl()
-
-    curl.setopt(pycurl.URL, url)
-    curl.setopt(pycurl.WRITEDATA, buffer)
-    curl.setopt(pycurl.SSL_VERIFYPEER, 0)
-    curl.perform()
-    response = buffer.getvalue()
-    response = json.loads(response.decode("iso-8859-1"))
-
-    if response.get("errors"):
-        raise Exception(response['errors'][0]["message"])
-    data = response['data']
-    return data['base'], data['currency'], data['amount']
+        data = await response.read()
+        json_data = json.loads(data)
+        if json_data.get("errors"):
+            raise Exception(json_data['errors'][0]["message"])
+        data = json_data['data']
+        return float(data['amount'])
 
 
-def crypto_graphic(start_date, end_date=datetime.today(), days_interval=30, base_currency="BTC", currency="USD"):
+async def crypto_graphic(start_date, end_date=datetime.today(), days_interval=30, base_currency="BTC", currency="USD"):
     # 'date_time' is a list of dates in string format.
     date_time = [start_date]
 
-    # 'data' is a list of float that represent the value of the crypto according to the ith date in 'date_time'.
-    start_amount = float(get_crypto_value(base_currency, currency, start_date)[-1])
-    data = [start_amount]
 
     date_format = "%Y-%m-%d"
 
     # while 'new_date' is less than 'end_date' keep adding dates and amounts to 'date_time' and 'data'.
 
-    curl = pycurl.Curl()
-    while True:
-        new_date = datetime.strptime(date_time[-1], date_format) + relativedelta(days=days_interval)
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        start_amount = asyncio.ensure_future(get_crypto_value(session, base_currency, currency, start_date))
+        tasks.append(start_amount)
 
-        if new_date > end_date:
-            end_date_str = end_date.strftime(date_format)
-            date_time.append(end_date_str)
-            amount = get_crypto_value(base_currency, currency, end_date_str, curl)[-1]
-            data.append(float(amount))
-            break
+        while True:
+            new_date = datetime.strptime(date_time[-1], date_format) + relativedelta(days=days_interval)
 
-        new_date_str = new_date.strftime(date_format)
-        date_time.append(new_date_str)
-        amount = get_crypto_value(base_currency, currency, new_date_str, curl)[-1]
-        data.append(float(amount))
+            if new_date > end_date:
+                end_date_str = end_date.strftime(date_format)
+                date_time.append(end_date_str)
+                async_func = asyncio.ensure_future(get_crypto_value(session, base_currency, currency, end_date_str))
+                tasks.append(async_func)
+                break
+
+            new_date_str = new_date.strftime(date_format)
+            date_time.append(new_date_str)
+            async_func = asyncio.ensure_future(get_crypto_value(session, base_currency, currency, new_date_str))
+            tasks.append(async_func)
+
+        data = await asyncio.gather(*tasks)
+
 
     # create a data frame with the amounts in the rows and the dates as index. Then plot it.
     date_time = pd.to_datetime(date_time, yearfirst=True)
@@ -71,5 +67,4 @@ def crypto_graphic(start_date, end_date=datetime.today(), days_interval=30, base
 
 
 
-crypto_graphic("2021-02-02", days_interval=1, base_currency="ETH")
-# print(get_crypto_value())
+asyncio.run(crypto_graphic("2021-02-02", days_interval=1, base_currency="BTC"))
