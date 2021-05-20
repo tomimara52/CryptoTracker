b06e86ed6aa105c19896c9ed8bf19392016b2d0b
Tomás Maraschio <tomas.maraschio@gmail.com>
2021-05-19 21:25:52 -0300

    added comments

--- main.py
+++ main.py
@@ -19,11 +19,16 @@ def get_crypto_value(base_currency="BTC", currency="USD", target_date=None):
 
 
 def crypto_graphic(start_date, end_date=datetime.today(), days_interval=30, base_currency="BTC", currency="USD"):
+    # 'date_time' is a list of dates in string format.
     date_time = [start_date]
+
+    # 'data' is a list of float that represent the value of the crypto according to the ith date in 'date_time'.
     start_amount = float(get_crypto_value(base_currency, currency, start_date)[-1])
     data = [start_amount]
+
     date_format = "%Y-%m-%d"
 
+    # while 'new_date' is less than 'end_date' keep adding dates and amounts to 'date_time' and 'data'.
     while True:
         new_date = datetime.strptime(date_time[-1], date_format) + relativedelta(days=days_interval)
 
@@ -39,11 +44,8 @@ def crypto_graphic(start_date, end_date=datetime.today(), days_interval=30, base
         amount = get_crypto_value(base_currency, currency, new_date_str)[-1]
         data.append(float(amount))
 
+    # create a data frame with the amounts in the rows and the dates as index. Then plot it.
     date_time = pd.to_datetime(date_time, yearfirst=True)
-    # for date_ in date_time:
-    #     amount = get_crypto_value(base_currency, currency, date_)[-1]
-    #     data.append(float(amount))
-
     DF = pd.DataFrame({"value": data})
     DF = DF.set_index(date_time)
     plt.plot(DF, "y")
